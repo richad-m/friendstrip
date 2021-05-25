0c9d991d6d85a7a95a57f8c66081cb07ac87218e
Iñaki Gauthier <inakigauthier92@gmail.com>
2021-05-25 11:59:16 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+trip1 = Trip.create(start_date:Date.new, end_date:Date.new, user_id:1, title:"best_in_spain")
+trip2 = Trip.create(start_date:Date.new, end_date:Date.new, user_id:2, title:"best_in_intaly")
+trip3 = Trip.create(start_date:Date.new, end_date:Date.new, user_id:3, title:"best_in_france")
+trip4 = Trip.create(start_date:Date.new, end_date:Date.new, user_id:4, title:"best_in_portugal")
+trip5 = Trip.create(start_date:Date.new, end_date:Date.new, user_id:5, title:"best_in_belgium")
+trip6 = Trip.create(start_date:Date.new, end_date:Date.new, user_id:6, title:"best_in_brazil")
+trip7 = Trip.create(start_date:Date.new, end_date:Date.new, user_id:7, title:"best_in_usa")
