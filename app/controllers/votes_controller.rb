47ebdc35ad9f808266b9534552d95a1b606e4db7
Richad MERALLI <richad.meralli@gmail.com>
2021-05-25 08:57:41 +0200

    commented all the controller and routes created too soon

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,34 +1,34 @@
 class VotesController < ApplicationController
+ #Richad a code trop vite ce controller
+  # def new
+  #   @vote = Vote.new
+  # end
 
-  def new
-    @vote = Vote.new
-  end
+  # def create
+  #   @vote = Vote.new(vote_params)
+  #   @proposition = Proposition.find(params[:proposition_id])
+  #   @vote.proposition_id = @proposition.id
+  #   @vote.user_id = current_user.id
+  #   if @vote.save
+  #     redirect_to trip_path(@proposition.trip_id)
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def create
-    @vote = Vote.new(vote_params)
-    @proposition = Proposition.find(params[:proposition_id])
-    @vote.proposition_id = @proposition.id
-    @vote.user_id = current_user.id
-    if @vote.save
-      redirect_to trip_path(@proposition.trip_id)
-    else
-      render :new
-    end
-  end
+  # def upvote
+  #   @vote = Vote.find(params[:id])
+  #   @vote.status = true
+  #   @vote.save
+  # end
 
-  def upvote
-    @vote = Vote.find(params[:id])
-    @vote.status = true
-    @vote.save
-  end
+  # def downvote
+  #   @vote = Vote.find(params[:id])
+  #   @vote.status = false
+  #   @vote.save
+  # end
 
-  def downvote
-    @vote = Vote.find(params[:id])
-    @vote.status = false
-    @vote.save
-  end
-
-  def vote_params
-    params.require(:vote).permit(:user_id, :proposition_id, :comment, :status)
-  end
+  # def vote_params
+  #   params.require(:vote).permit(:user_id, :proposition_id, :comment, :status)
+  # end
 end
