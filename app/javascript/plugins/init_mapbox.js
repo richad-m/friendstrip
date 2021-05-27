e4495d87a4bda6fe4fa5a721ce95b0c60362f012
clotilde lacaille <lacailleclotilde@gmail.com>
2021-05-27 11:13:53 +0200

    map style done

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,7 +5,7 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/clotildelacaille/ckowykosf11l418oahozxlsmj'
   });
 };
 
