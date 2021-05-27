a18b9a0296f6830b98c7d568a034972a90805b7a
Richad MERALLI <richad.meralli@gmail.com>
2021-05-27 12:37:35 +0200

    added nb of likes and dislikes in show trip

--- app/models/proposition.rb
+++ app/models/proposition.rb
@@ -1,6 +1,7 @@
 class Proposition < ApplicationRecord
   belongs_to :trip
   belongs_to :user
+  has_many :votes
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
