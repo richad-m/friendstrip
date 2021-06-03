30fcf47ed0129f3aaee2086f995d0de8b436bb1b
Richad MERALLI <richad.meralli@gmail.com>
2021-06-03 09:19:34 +0200

    made validations active record

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,8 @@
 class Trip < ApplicationRecord
   belongs_to :user
-  has_many :propositions
+  has_many :propositions, dependent: :destroy
   has_many :invites
   has_many :users, through: :invites
+
+  validates :start_date, :end_date, :destination, presence: true
 end
